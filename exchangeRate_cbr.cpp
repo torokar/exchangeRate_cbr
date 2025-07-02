de4764fb305ddab232805cf310bc1594316aeb45
Andrey Onkhonov <148449110+torokar@users.noreply.github.com>
2025-07-02 18:46:20 +0800

    Update exchangeRate_cbr.cpp

--- exchangeRate_cbr.cpp
+++ exchangeRate_cbr.cpp
@@ -19,19 +19,6 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmeb, void* user
 	((std::string*)userp)->append((char*)contents, size * nmeb);
 	return size * nmeb;
 }
-/*
-	void*contents - это пачка данных от сервера. Сервер присылает данные не целмком
-	а отдельными кусочками
-	size_t size и size_t nmeb - размер одного данных (в байтах 1)
-	nmeb - сколько таких кусочков пришло
-	Вместе size * nmemb - это общий размер данных в этой "пачке"
-
-	void* userp - это коробка для писем в нашем случае std::string куда мы будем складывать данные
-
-	((std::string*)userp)->append - append добавляет новые данные в конец строки (char*)contents
-	преобразует данные в текст
-	size * nmeb - сколько байт нужно добавить
-*/
 
 std::string encode_date(const std::string& date)
 {
@@ -60,9 +47,7 @@ int main()
 	//Выполняем запрос
 	CURLcode resultat; /*Объект куда будет записанны данные*/
 	curl = curl_easy_init();
-
-
-	std::cout << "Формат ввода даты **02/03/2002**";
+	
 	date = "02/03/2002";
 	//std::cout << "\nВведите дата: ";
 	//std::getline(std::cin, date);
@@ -99,13 +84,6 @@ int main()
 
 
 	SubstrCurrensiFromXML(XmlData, DataCurr);
-
-	std::cout << DataCurr[0].Date << std::endl;
-	for (int i = 0; i < DataCurr.size(); i++)
-	{
-		std::cout << DataCurr[i].CharCode << " " << DataCurr[i].Name_currence << " " << DataCurr[i].Value << "\n";
-	}
-
 	WriteFile(DataCurr);
 	ConnectedBD(DataCurr);
 
